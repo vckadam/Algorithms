e3209cb89cd07aa95852131b286726b91244ea53
vckadam <vckadam@asu.edu>
2017-04-02 22:21:55 -0700

    Optimized 01matrix

--- Recent/_01matrix.java
+++ Recent/_01matrix.java
@@ -61,3 +61,38 @@ public class Solution {
         return;
     }
 }
+
+/////////////// Another vesion
+
+public class Solution {
+    public List<List<Integer>> updateMatrix(List<List<Integer>> matrix) {
+        for(int i = 0; i < matrix.size(); i++) {
+            for(int j = 0; j < matrix.get(i).size(); j++) {
+                if(matrix.get(i).get(j) != 0) matrix.get(i).set(j,Integer.MAX_VALUE);
+            }
+        }
+        for(int i = 0; i < matrix.size(); i++) {
+            for(int j = 0; j < matrix.get(i).size(); j++) {
+                if(matrix.get(i).get(j) != 0) {
+                    continue;
+                }
+                dfs(matrix, i+1, j, 1);
+                dfs(matrix, i-1, j, 1);
+                dfs(matrix, i, j-1, 1);
+                dfs(matrix, i, j+1, 1);
+            }
+        }
+        return matrix;
+    }
+    public void dfs(List<List<Integer>> matrix, int i, int j, int count) {
+        if(i < 0 || j < 0 || i == matrix.size() || j == matrix.get(i).size() || matrix.get(i).get(j) == -1 || matrix.get(i).get(j) == 0) return;
+        int temp = matrix.get(i).get(j);
+        matrix.get(i).set(j,-1);
+        if(i < matrix.size()-1 && matrix.get(i+1).get(j) > count+1) dfs(matrix,i+1,j,count+1);
+        if(j > 0 && matrix.get(i).get(j-1) > count+1) dfs(matrix,i,j-1,count+1);
+        if(i > 0 && matrix.get(i-1).get(j) > count+1) dfs(matrix,i-1,j,count+1);
+        if(j < matrix.get(i).size()-1 && matrix.get(i).get(j+1) > count+1) dfs(matrix,i,j+1,count+1);
+        matrix.get(i).set(j,Math.min(temp,count));
+        return;
+    }
+}
