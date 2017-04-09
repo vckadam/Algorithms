bb51aabbe17a166b08db2e5c57ecc62006b9904b
vckadam <vckadam@asu.edu>
2017-04-08 18:40:20 -0700

    Added solution to avoid deadlock

--- Recent/DeadLockExample.java
+++ Recent/DeadLockExample.java
@@ -52,3 +52,75 @@ public class HelloWorld{
         }).start();
      }
 }
+/*
+output : 
+Inside method1
+Inside method2
+*/)
+/////////////////// solution to deadlock
+always aquire lock in same order 
+class MyClass {
+    public void method1(MyClass2 obj) throws InterruptedException{
+        synchronized(this) {
+            synchronized(obj){
+                System.out.println("Inside method1");
+                try { Thread.sleep(4000); } catch(Exception e) {System.out.println(e);}
+                obj.method4();
+            }
+        }
+    }
+    public void method3() {
+        synchronized(this) {
+            System.out.println("Inside method3");
+        }
+    }
+}
+class MyClass2 {
+    public void method2(MyClass obj) throws InterruptedException {
+        synchronized(obj) {
+            synchronized(this) {
+                System.out.println("Inside method2");
+                try { Thread.sleep(4000); } catch(Exception e) {System.out.println(e);}
+                obj.method3();
+            }
+        }
+    }
+    public synchronized void method4() {
+        synchronized(this) {
+            System.out.println("Inside method4");
+        }
+    }
+}
+public class HelloWorld{
+
+     public static void main(String []args) throws InterruptedException{
+        System.out.println("Hello World");
+        MyClass obj = new MyClass();
+        MyClass2 obj2 = new MyClass2();
+        new Thread(new Runnable() {
+            public void run() {
+                try{
+                obj.method1(obj2);
+                }catch(Exception e) {
+                    System.out.println(e);
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            public void run() {
+                try{
+                obj2.method2(obj);
+                }catch(Exception e) {
+                    System.out.println(e);
+                }
+            }
+        }).start();
+     }
+}
+/*
+output :
+Inside	method1                                                                                                                               
+Inside method4                                                                                                                               
+Inside method2                                                                                                                               
+Inside method3  
+*/
