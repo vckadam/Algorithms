38f16bc16dfe8576bdf3f49c146ef6f3a528caf5
vckadam <vckadam@asu.edu>
2017-06-24 12:44:57 -0700

    Leetcode 623. Add One Row to Tree

---
+++ Recent/AddOneRowtoTree.java
@@ -0,0 +1,98 @@
+/*
+623. Add One Row to Tree
+
+Given the root of a binary tree, then value v and depth d, you need to add a row of nodes with value v at the given depth d. The root node is at depth 1.
+
+The adding rule is: given a positive integer depth d, for each NOT null tree nodes N in depth d-1, create two tree nodes with value v as N's left subtree root and right subtree root. And N's original left subtree should be the left subtree of the new left subtree root, its original right subtree should be the right subtree of the new right subtree root. If depth d is 1 that means there is no depth d-1 at all, then create a tree node with value v as the new root of the whole original tree, and the original tree is the new root's left subtree.
+
+Example 1:
+Input: 
+A binary tree as following:
+       4
+     /   \
+    2     6
+   / \   / 
+  3   1 5   
+
+v = 1
+
+d = 2
+
+Output: 
+       4
+      / \
+     1   1
+    /     \
+   2       6
+  / \     / 
+ 3   1   5   
+
+Example 2:
+Input: 
+A binary tree as following:
+      4
+     /   
+    2    
+   / \   
+  3   1    
+
+v = 1
+
+d = 3
+
+Output: 
+      4
+     /   
+    2
+   / \    
+  1   1
+ /     \  
+3       1
+Note:
+The given d is in range [1, maximum depth of the given tree + 1].
+The given binary tree has at least one tree node.
+
+ */
+ 
+ /**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode addOneRow(TreeNode root, int v, int d) {
+        if(d == 1) {
+            TreeNode temp = new TreeNode(v);
+            temp.left = root;
+            return temp;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        queue.offer(null);
+        int level = 1;
+        while(!queue.isEmpty()) {
+            TreeNode temp = queue.poll();
+            if(temp != null) {
+                if(level == d - 1) {
+                    TreeNode left = new TreeNode(v);
+                    TreeNode right = new TreeNode(v);
+                    left.left = temp.left;
+                    right.right = temp.right;
+                    temp.left = left;
+                    temp.right = right;
+                } else {
+                    if(temp.left != null) queue.offer(temp.left);
+                    if(temp.right != null) queue.offer(temp.right);
+                }
+            } else {
+                level++;
+                if(!queue.isEmpty()) queue.offer(null);
+            }
+        }
+        return root;
+    }
+}
