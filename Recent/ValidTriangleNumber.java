f77167fcc2dc6ffcc7e836e9d4b1c52d10dd5d7d
vckadam <vckadam@asu.edu>
2017-06-26 20:11:49 -0700

    Update - Leetcode 611. Valid Triangle Number

--- Recent/ValidTriangleNumber.java
+++ Recent/ValidTriangleNumber.java
@@ -16,6 +16,27 @@ The length of the given array won't exceed 1000.
 The integers in the given array are in the range of [0, 1000].
 
 */
+// Optimized 
+public class Solution {
+    public int triangleNumber(int[] nums) {
+        if(nums.length <= 2) return 0;
+        Arrays.sort(nums);
+        int count = 0;
+        for(int k = nums.length-1; k >= 2; k--) {
+            int left = 0, right = k - 1;
+            while(left < right) {
+                if(nums[left]+nums[right] > nums[k]) {
+                    count += (right - left);
+                    right--;
+                } 
+                else left++;
+            }
+        }
+        return count;
+    }
+}
+
+// Another 
 public class Solution {
     public int triangleNumber(int[] nums) {
         if(nums.length <= 2) return 0;
