3ce07e6e9be9a5121b93fd108dfd6d89f822e44a
vckadam <vckadam@asu.edu>
2017-07-20 20:57:20 -0700

    Tested ReverseBit bitManipultion

--- Recent/BookProblems/src/bitmanipulation/test/ReverseBitTest.java
+++ Recent/BookProblems/src/bitmanipulation/test/ReverseBitTest.java
@@ -25,6 +25,27 @@ public class ReverseBitTest {
 	@Test
 	public void getReversedBitNumberTest1() {
 		int actualNumber = this.reverseBit.getReversedBitNumber(1);
-		assertEquals(-2147483648, actualNumber);
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < 32; i++) {
+			sb.insert(0, (actualNumber&(1<<i)) == 0 ? 0 : 1);
+		}
+		//System.out.println(sb.toString());
+		assertEquals("10000000000000000000000000000000", sb.toString());
+	}
+	
+	@Test
+	public void getReversedBitNumberTest2() {
+		int actualNumber = this.reverseBit.getReversedBitNumber(0);
+		assertEquals(0, actualNumber);
+	}
+	
+	@Test
+	public void getReversedBitNumberTest3() {
+		int actualNumber = this.reverseBit.getReversedBitNumber(2);
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < 32; i++) {
+			sb.insert(0, (actualNumber&(1<<i)) == 0 ? 0 : 1);
+		}
+		assertEquals("01000000000000000000000000000000", sb.toString());
 	}
 }
