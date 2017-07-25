79947806a608baa3adf38c74692f8951fd2bedbc
vckadam <vckadam@asu.edu>
2017-07-24 23:16:31 -0700

    HackerRank string java string compare

---
+++ Recent/Hacker_rank/String/JavaSubstringComparisons.java
@@ -0,0 +1,73 @@
+/*
+Java Substring Comparisons
+
+We define the following terms:
+
+Lexicographical Order, also known as alphabetic or dictionary order, orders characters as follows: 
+For example, ball < cat, dog < dorm, Happy < happy, Zoo < ball.
+
+A substring of a string is a contiguous block of characters in the string. For example, the substrings of abc are a, b, c, ab, bc, and abc.
+Given a string, , and an integer, , complete the function so that it finds the lexicographically smallest and largest substrings of length .
+
+Input Format
+
+The first line contains a string denoting . 
+The second line contains an integer denoting .
+
+Constraints
+
+ consists of English alphabetic letters only (i.e., [a-zA-Z]).
+Output Format
+
+Return the respective lexicographically smallest and largest substrings as a single newline-separated string.
+
+Sample Input 0
+
+welcometojava
+3
+Sample Output 0
+
+ava
+wel
+Explanation 0
+
+String  has the following lexicographically-ordered substrings of length :
+
+We then return the first (lexicographically smallest) substring and the last (lexicographically largest) substring as two newline-separated values (i.e., ava\nwel).
+
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+  
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        /* abcde - 2
+           01234
+           0 -> 3 (5 - 2)
+        */  
+        String subString = s.substring(0,k);
+        smallest = largest = subString;
+        for(int i = 1; i <= s.length()-k; i++) {
+            subString = s.substring(i,i+k);
+            if(subString.compareTo(smallest) < 0) smallest = subString;
+            if(subString.compareTo(largest) > 0) largest = subString;
+        }
+        return smallest + "\n" + largest;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
