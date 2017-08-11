e1bc19dfb3e6a63d4082c820f0c54928be1c89a9
vckadam <vckadam@asu.edu>
2017-08-10 20:27:39 -0400

    Implemented & Tested Floor number search Book

---
+++ Recent/BookProblems/src/search/FloorAndCeiling.java
@@ -0,0 +1,25 @@
+package search;
+
+public class FloorAndCeiling {
+	public int getFloor(int[] array, int num) {
+		/*  L R
+		 *  2 4  - in between
+		 *  1 2 - both less
+		 *  4 5 - both grater
+		 *  3
+		 *  mid = left + (left + right + 1)/2;
+		 */
+		if(array == null || array.length == 0)
+			throw new IllegalArgumentException("Illegal Argument");
+		if(array[0] > num)
+			return -1;
+		int left = 0, right = array.length - 1; 
+		while(left < right) {
+			int mid = (right + left + 1) >> 1;
+			if(num == array[mid]) return num;
+			else if(num < array[mid]) right = mid - 1;
+		    else left = mid;
+		}
+		return array[left];
+	}
+}
---
+++ Recent/BookProblems/test/search/FloorAndCeilingTest.java
@@ -0,0 +1,107 @@
+package search;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FloorAndCeilingTest {
+
+	private FloorAndCeiling floorAndCeiling;
+	@Before
+	public void setUp() throws Exception {
+		this.floorAndCeiling = new FloorAndCeiling();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		this.floorAndCeiling = null;
+	}
+
+	@Test
+	public void testGetFloor_BasicScenario() {
+		int[] array = {1, 3};
+		assertEquals(1, this.floorAndCeiling.getFloor(array, 2));
+	}
+	
+	@Test
+	public void testGetFloor_NumPresent() {
+		int[] array = {1, 3};
+		assertEquals(3, this.floorAndCeiling.getFloor(array, 3));
+	}
+	
+	@Test
+	public void testGetFloor_NumPresentFirstPos() {
+		int[] array = {1, 3};
+		assertEquals(1, this.floorAndCeiling.getFloor(array, 1));
+	}
+	
+	@Test
+	public void testGetFloor_NumLessThanAll() {
+		int[] array = {1, 3};
+		assertEquals(-1, this.floorAndCeiling.getFloor(array, 0));
+	}
+	
+	@Test
+	public void testGetFloor_NumGreaterThanAll() {
+		int[] array = {1, 3};
+		assertEquals(3, this.floorAndCeiling.getFloor(array, 5));
+	}
+	
+	@Test(timeout = 100)
+	public void testGetFloor_OddLengthNumberNotPresent() {
+		int[] array = {1, 3, 5};
+		assertEquals(3, this.floorAndCeiling.getFloor(array, 4));
+	}
+	
+	@Test
+	public void testGetFloor_OddLengthNumberPresent() {
+		int[] array = {1, 3, 5};
+		assertEquals(3, this.floorAndCeiling.getFloor(array, 3));
+	}
+	
+	@Test(timeout = 100)
+	public void testGetFloor_Duplicate() {
+		int[] array = {1, 1, 3, 3, 5, 5};
+		assertEquals(3, this.floorAndCeiling.getFloor(array, 4));
+	}
+	
+	@Test(timeout = 100)
+	public void testGetFloor_NegativeEle() {
+		int[] array = {-2, 0, 1, 1, 3, 3, 5, 5};
+		assertEquals(-2, this.floorAndCeiling.getFloor(array, -1));
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testGetFloor_NullInput() {
+		int[] array = null;
+		assertEquals(3, this.floorAndCeiling.getFloor(array, 4));
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testGetFloor_EmptyArray() {
+		int[] array = {};
+		assertEquals(3, this.floorAndCeiling.getFloor(array, 4));
+	}
+	
+	@Test
+	public void testGetFloor_SingleElementArray() {
+		int[] array = {2};
+		assertEquals(2, this.floorAndCeiling.getFloor(array, 4));
+	}
+	
+	@Test(timeout = 100)
+	public void testGetFloor_SmallestInteger() {
+		int[] array = {-2147483648, 1, 1, 3, 3, 5, 5, 2147483647};
+		assertEquals(-2147483648, this.floorAndCeiling.getFloor(array, -2147483648));
+	}
+	
+	@Test(timeout = 100)
+	public void testGetFloor_LargestInteger() {
+		int[] array = {-2147483648, 1, 1, 3, 3, 5, 5, 2147483647};
+		assertEquals(2147483647, this.floorAndCeiling.getFloor(array, 2147483647));
+	}
+	
+
+}
