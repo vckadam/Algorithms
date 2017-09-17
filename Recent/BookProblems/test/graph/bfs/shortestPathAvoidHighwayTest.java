d742ec30d053a1fc7610dca85e8882c37c87005d
vckadam <vckadam@asu.edu>
2017-09-16 20:22:04 -0400

    Added test cases for shortest path avoiding highways graph bfs

--- Recent/BookProblems/test/graph/bfs/shortestPathAvoidHighwayTest.java
+++ Recent/BookProblems/test/graph/bfs/shortestPathAvoidHighwayTest.java
@@ -30,5 +30,41 @@ public class shortestPathAvoidHighwayTest {
 		String expectedPath = "0->2";
 		assertEquals(expectedPath, actualPath);
 	}
+	
+	@Test
+	public void testGetSortestPath_GraphWithCyclesNoHighWays() {
+		int[][] edges = {{0,1},{0,2},{1,2},{2,3},{2,4},{2,9},{3,5},{4,5},{4,7},{5,6},{5,7},{6,8},{7,8},{8,9}};
+		int[][] highways = {};
+		String actualPath = this.shortestPathAvoidHighway.getSortestPath(10, edges, highways, 0, 9);
+		String expectedPath = "0->2->9";
+		assertEquals(expectedPath, actualPath);
+	}
+	
+	@Test
+	public void testGetSortestPath_GraphWithCyclesOneHighWays() {
+		int[][] edges = {{0,1},{0,2},{1,2},{2,3},{2,4},{2,9},{3,5},{4,5},{4,7},{5,6},{5,7},{6,8},{7,8},{8,9}};
+		int[][] highways = {{2,9}};
+		String actualPath = this.shortestPathAvoidHighway.getSortestPath(10, edges, highways, 0, 9);
+		String expectedPath = "0->2->4->7->8->9";
+		assertEquals(expectedPath, actualPath);
+	}
+	
+	@Test
+	public void testGetSortestPath_GraphWithCyclesTwoHighWays() {
+		int[][] edges = {{0,1},{0,2},{1,2},{2,3},{2,4},{2,9},{3,5},{4,5},{4,7},{5,6},{5,7},{6,8},{7,8},{8,9}};
+		int[][] highways = {{2,9},{4,7}};
+		String actualPath = this.shortestPathAvoidHighway.getSortestPath(10, edges, highways, 0, 9);
+		String expectedPath = "0->2->3->5->6->8->9";
+		assertEquals(expectedPath, actualPath);
+	}
+	
+	@Test
+	public void testGetSortestPath_NoPathExist() {
+		int[][] edges = {{0,1},{0,2},{1,2},{2,3},{2,4},{2,9},{3,5},{4,5},{4,7},{5,6},{5,7},{6,8},{7,8},{8,9}};
+		int[][] highways = {{2,9},{4,7},{8,9}};
+		String actualPath = this.shortestPathAvoidHighway.getSortestPath(10, edges, highways, 0, 9);
+		String expectedPath = null;
+		assertEquals(expectedPath, actualPath);
+	}
 
 }
